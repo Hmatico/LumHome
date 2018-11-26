04e9bf0be08f9ae3a4c1b580b61809f55e50ad4e
Hmatico <mathieu.valentin1@orange.fr>
2018-11-26 20:25:16 +0100

    MVC php js jquery et ajax début++

--- Site/vue/javaScript/front.js
+++ Site/vue/javaScript/front.js
@@ -5,13 +5,21 @@ $(document).ready(function(){
         console.log("pwd: "+ $("#pwd").val());
         
         if($("#email").val() != "" && $("#pwd").val() != ""){
+            var controler = "controle=gestionSession&action=ident";
+            var email_content = "login="+$("#email").val();
+            var pwd_content = "pwd="+$("#pwd").val();
             $.ajax({
                 type: "POST",
                 url: "../index.php",
-                data: "controle=gestionSession&action=ident&login="+$("#email").val()+"&pwd="+$("#pwd").val(),
+                data: controler+"&"+email_content+"&"+pwd_content,
                 success: function(result){
                     if(result){
                         alert(result);
+                    }
+                },
+                error: function(result){
+                    if(result){
+                        console.log(result);
                     }
                 }
             });
