c6d9f461e3cb1e249535bebf6a5be871533eeba4
Hmatico <mathieu.valentin1@orange.fr>
2018-11-27 00:46:10 +0100

    modif message d'alert et dodo

--- Site/vue/javaScript/front.js
+++ Site/vue/javaScript/front.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
                 }
             });
         } else {
-            alert("haaalo");
+            alert("Au moins un des champs est vide !");
         }
     });
     
