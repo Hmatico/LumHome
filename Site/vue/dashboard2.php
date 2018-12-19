8a89f5e5ddac37f5fb13e5caa4cc4d10907842a5
fferwan <desaint.erwan@gmail.com>
2018-12-19 23:35:00 +0100

    putain il est stylé le dashboard

--- Site/vue/dashboard2.php
+++ Site/vue/dashboard2.php
@@ -3,17 +3,10 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, maximum-scale=1"/>
 	<title>Dashboard</title>
-	<link rel="stylesheet" href="dashboard2.css">
+	<link rel="stylesheet" href="hello.css">
 	<link rel="stylesheet" href="ui.colorpicker.css"/>
 	<script language="JavaScript" src="jQuery.js"></script>
 	<script language="JavaScript" src="jQuery.colorpicker.js"></script>
-  </head>
-  <body>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>LumHome</title>
     </head>
     <body>
 	<?php include "entete.html";?>
@@ -28,41 +21,53 @@
 				</form>
 			</div>
 			<div class="dtteteindre">
-				<label class="switch"><input type="checkbox"><span class="slider round"></span></label>
-				<a class="dtxteteindre"> Tout éteindre</a>
+				<div class="dtxteteindre"> Tout éteindre</div>
+				<label class="switch"><input type="checkbox" checked><span class="slider round"></span></label>
 			</div>
 		</div>
 		<div class="drightcontainer">
-			<div class="dligne">
-				<ul>
-					<li><div class="detatpiece">Chambre de Didier : <a class = "detat"> allo </a></div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece: <a class = "detat"> allumé </a></div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-				</ul>
+			<div class ="ligne">
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
 			</div>
-			<div class="dligne">
-				<ul>
-					<li><div class="detatpiece">piece(Noël)</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piedgophlhhce: <a class = "detat"> Noël </a></div></li>
-					<li><div class="detatpiece">piece</div></li>
-				</ul>
+			<div class ="ligne">
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
 			</div>
-			<div class="dligne">
-				<ul>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-					<li><div class="detatpiece">piece</div></li>
-				</ul>
+			<div class ="ligne">
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
+				<div class="detatpiece">Piece : <a class = "detat"> état </a></div>
 			</div>
 		</div>
 	</div>
+	<div class ="scenario">
+			<div class="Piece">
+						<div class="Entete_piece"> Chambre de David </div>
+						<div class="contenu_piece">
+							<div class="leftpiece"> 
+								<div class="div_colorpicker"><input type="color" class="colorpicker"></div>
+								<div class="texte_couleur">Couleur</div>
+							</div>
+							<div class="rightpiece">
+							<img src="./resources/soleil.png" class = "soleil"><br><a class="texteluminosite">Luminosité</a>
+							</div>
+							<div><input class="piecerange" type="range"/>
+								<div class="texte_intensite">Intensité</div></div>
+
+						</div>
+	</div>
+  </div>
+</body>
 </html>
