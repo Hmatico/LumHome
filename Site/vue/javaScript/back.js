356fb794ee72dfc58ac84de16f6e577d86b74087
Hmatico <mathieu.valentin1@orange.fr>
2019-01-18 10:20:19 +0100

    modif index

--- Site/vue/javaScript/back.js
+++ Site/vue/javaScript/back.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
                             $(".modal .modal-content p").html("Identifiant ou mot de passe incorrect !");
                             $(".modal").css("display","block");
                         } else {
+                            console.log(data);
                             connexion(data);
                         }
                     }
