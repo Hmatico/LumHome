5c970ff67c97b6d93009d9fef2b840a53d48dd08
fferwan <desaint.erwan@gmail.com>
2019-01-22 23:23:48 +0100

    entete change le css selon sur quoi t'as cliqué

--- Site/vue/javaScript/entete.js
+++ Site/vue/javaScript/entete.js
@@ -10,18 +10,41 @@ $(document).ready(function(){
 	);
     $('button').click(function(){
 		var lien = $(this).attr("class");
-
+		document.getElementsByClassName("boutonclic")[0].classList.add("boutonpasclic");
+		document.getElementsByClassName("txtclic")[0].classList.add("txtpasclic");
+		document.getElementsByClassName("boutonclic")[0].classList.remove("boutonclic");
+		document.getElementsByClassName("txtclic")[0].classList.remove("txtclic");
+		
 		if (lien == "btnaccueil") {
 			var page="dashboard2.php";
+			document.getElementsByClassName("boutonpasclic")[0].classList.add("boutonclic");
+			document.getElementsByClassName("txtpasclic")[0].classList.add("txtclic");
+			document.getElementsByClassName("boutonpasclic")[0].classList.remove("boutonpasclic");
+			document.getElementsByClassName("txtpasclic")[0].classList.remove("txtpasclic");
 		}
 		if (lien == "btnscenario") {
 			var page="gerer_sc.html";
+			document.getElementsByClassName("boutonpasclic")[1].classList.add("boutonclic");
+			document.getElementsByClassName("txtpasclic")[1].classList.add("txtclic");
+			document.getElementsByClassName("boutonpasclic")[1].classList.remove("boutonpasclic");
+			document.getElementsByClassName("txtpasclic")[1].classList.remove("txtpasclic");
+
 		}
 		if (lien == "btngerer") {
 			var page="gerer.php";
+			document.getElementsByClassName("boutonpasclic")[2].classList.add("boutonclic");
+			document.getElementsByClassName("txtpasclic")[2].classList.add("txtclic");
+			document.getElementsByClassName("boutonpasclic")[2].classList.remove("boutonpasclic");
+			document.getElementsByClassName("txtpasclic")[2].classList.remove("txtpasclic");
+
 		}
 		if (lien == "btnstatistique") {
 			var page="statistiques.php";
+			document.getElementsByClassName("boutonpasclic")[3].classList.add("boutonclic");
+			document.getElementsByClassName("txtpasclic")[3].classList.add("txtclic");
+			document.getElementsByClassName("boutonpasclic")[3].classList.remove("boutonpasclic");
+			document.getElementsByClassName("txtpasclic")[3].classList.remove("txtpasclic");
+
 		}
 		$("#suite").load(
 			page,
