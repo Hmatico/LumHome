3831b5aff1fba931c8c1265fcf696368d20a7912
Hmatico <mathieu.valentin1@orange.fr>
2019-06-11 16:23:40 +0200

    ajouts fcts passerelles

---
+++ Site/controle/gateway.php
@@ -0,0 +1,49 @@
+<?php
+
+function getLogs(){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"http://projets-tomcat.isep.fr:8080/appService/?ACTION=GETLOG&TEAM=A02B");
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    
+    $moteur = "";
+    $lum = "";
+
+    $data_tab = str_split($data,33);
+    for($i=count($data_tab) - 1;$i>=0;$i--){
+        list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($data_tab[$i],"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+        if($t != null){
+            if($moteur == ""){
+                if($n=="02" || $n=="04"){
+                    $moteur = $v;
+                }
+            }
+            if($lum == ""){
+                if($n=="01" || $n=="03"){
+                    $lum = $v;
+                }
+            }
+        }
+    }
+    require("modele/gateway.php");
+    sql_request("moteur",hexdec($moteur));
+    sql_request("lumiere",hexdec($lum));
+}
+
+
+function sendCommand($trame){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://projets-tomcat.isep.fr:8080/appService/?ACTION=COMMAND&TEAM=A02B&TRAME=".$trame);
+    curl_setopt($ch, CURLOPT_POST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $ans = curl_exec($ch); 
+ 
+    curl_close($ch);
+    
+    echo "<br /> Réponse : ";
+    echo htmlspecialchars($ans);
+}
+
+?>
