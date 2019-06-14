20b65cb5d1afdc6ca6ceb0495d6684d831c9b31b
fferwan <desaint.erwan@gmail.com>
2019-06-14 16:45:51 +0200

    ajout modif intensite

--- Site/vue/javaScript/dashboard.js
+++ Site/vue/javaScript/dashboard.js
@@ -117,12 +117,29 @@ function ModifierCouleur(clicked_id){
 
 function ModifierIntensite(clicked_id){
 	var controler = "controle=dashboard&action=ModifierIntensite";
+	var controler2 = "controle=gateway&action=sendCommand";
+	var trame = '';
 	var piece = "piece="+clicked_id[clicked_id.length -4]+clicked_id[clicked_id.length -3]+clicked_id[clicked_id.length -2]+clicked_id[clicked_id.length -1];
 	var intensite = "intensite="+document.getElementById(clicked_id).value;
+	if(document.getElementById(clicked_id).value < 10)
+	{
+		intensite = 'intensite=00'.document.getElementById(clicked_id).value;
+	}
+	else if(document.getElementById(clicked_id).value < 100)
+	{
+		intensite = 'intensite=0'.document.getElementById(clicked_id).value;
+	}
+	trame = 'trame=1A02B1a03'.intensite.'65';
 	var dataPOST = controler+"&"+piece+"&"+intensite;
+	var dataPOST2 = controler2+"&"+trame;
 	$.ajax({
 		type: "POST",
 		url: "../index.php",
 		data: dataPOST,
+	});
+		$.ajax({
+		type: "POST",
+		url: "../index.php",
+		data: dataPOST2,
 	});
 }
