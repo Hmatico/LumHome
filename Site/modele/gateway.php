c7139c20ef9b4786fef4dadae7595310c3e75d8e
Hmatico <mathieu.valentin1@orange.fr>
2019-06-17 15:34:11 +0200

    modif num serie ds controler

--- Site/modele/gateway.php
+++ Site/modele/gateway.php
@@ -1,10 +1,10 @@
 <?php
 
-function sql_request($type, $value)
+function sql_request($id, $type, $value)
 {
     require("modele/connexionBD.php");
-    $update =   "UPDATE cemac SET intensite = '%s' WHERE type = '%s'";
-    $req = sprintf($update, $value, $type);
+    $update =   "UPDATE cemac SET intensite = '%s' WHERE type = '%s' and numeroSerie='%s'";
+    $req = sprintf($update, $value, $type,$id);
     $res = mysqli_query($link, $req)   
             or die (utf8_encode("erreur de requête : ") . $req .'\n'.mysqli_error($link));
 }
